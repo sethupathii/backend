e689f267477fcaf50b565a225946a268cf4d6c4e
Sethupathi <mysethu888@gmail.com>
2023-12-19 15:10:11 +0530

    first commit

---
+++ apps.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+app.use(cors());
+const Port = process.env.PORT;
+
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(Port, (req, res) => {
+    console.log("THe port is running at the port of 6500");
+})
+
+const URL = process.env.Mongo_DB || 6500;
+const dbName = 'Todo';
+
+mongoose.connect(URL, {
+    dbName: dbName
+}).then(() => {
+    console.log("mongoose connected successfully");
+}).catch(err => console.log(err));
